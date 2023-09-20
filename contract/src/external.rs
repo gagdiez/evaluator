4c8dcec2b0d70ef350c88639208a667364515f52
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2023-09-20 21:11:16 +0300

    add sub_account checcker

--- contract/src/external.rs
+++ contract/src/external.rs
@@ -1,7 +1,6 @@
 use near_sdk::ext_contract;
 
 // Validator interface, for cross-contract calls
-// why does this value say hello_near and the trait says HelloNear?
 #[ext_contract(hello_near)]
 trait HelloNear {
     fn get_greeting(&self) -> String;
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
-use near_sdk::env::{predecessor_account_id, random_seed, signer_account_id};
+use near_sdk::env::{predecessor_account_id, random_seed};
 use near_sdk::serde_json::json;
 use near_sdk::{env, log, near_bindgen, AccountId, Gas, Promise, PromiseError};
 
@@ -19,32 +19,30 @@ pub struct Contract {
 
 impl Default for Contract {
     fn default() -> Self {
-        env::panic(b"The contract is not initialized.")
+        Self {
+            records: LookupMap::new(b"r".to_vec()),
+        }
     }
 }
 
 #[near_bindgen]
 impl Contract {
-    #[init]
-    pub fn new() -> Self {
-        // Useful snippet to copy/paste, making sure state isn't already initialized
-        assert!(env::state_read::<Self>().is_none(), "Already initialized");
-        // Note this is an implicit "return" here
-        Self {
-            records: LookupMap::new(b"r".to_vec()),
-        }
-    }
-
     // Public - query external greeting
-    pub fn evaluate_hello_near(&mut self, contract_name: AccountId) -> Promise {
-        // // First let's get a random string from random seed
+    pub fn evaluate_hello_near(&mut self, contract_account_id: AccountId) -> Promise {
+        assert!(
+            self.evaluating_sub_account(&contract_account_id),
+            "Please deploy contract as sub account. such as hello_near.{}",
+            contract_account_id
+        );
+
+        // First let's get a random string from random seed
         let get_array: Vec<u8> = random_seed();
         let random_string: String = String::from_utf8_lossy(&get_array).to_string();
         println!("the random string is {:?}", random_string);
 
         let args = json!({ "message": random_string }).to_string().into_bytes();
 
-        Promise::new(contract_name.clone())
+        Promise::new(contract_account_id.clone())
             .function_call("set_greeting".to_string(), args, NO_DEPOSIT, Gas(5 * TGAS))
             .function_call(
                 "get_greeting".to_string(),
@@ -55,7 +53,7 @@ impl Contract {
             .then(
                 Self::ext(env::current_account_id())
                     .with_static_gas(Gas(5 * TGAS))
-                    .evaluate_hello_near_callback(random_string, contract_name.clone()),
+                    .evaluate_hello_near_callback(random_string, contract_account_id.clone()),
             )
     }
 
@@ -78,7 +76,15 @@ impl Contract {
         }
     }
 
-    pub fn account_participation(&self, account_name: AccountId) -> bool {
+    pub fn account_participation(&self, account_name: &AccountId) -> bool {
         self.records.get(&account_name).unwrap_or(false)
+    }
+
+    // Account ID that's being checked is Sub-Account of the caller
+    #[private]
+    pub fn evaluating_sub_account(&self, account_id: &AccountId) -> bool {
+        predecessor_account_id()
+            .as_str()
+            .contains(&account_id.as_str())
     }
 }
