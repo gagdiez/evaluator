9c48f94a4b056378c54a47b0e35b2a9dabe89765
gagdiez <gagdiez.c@gmail.com>
2023-11-10 12:47:20 +0000

    changed expected input/outpjut

--- contract/src/eval_complex_input.rs
+++ contract/src/eval_complex_input.rs
@@ -12,19 +12,36 @@ use crate::{
     Contract, ContractExt,
 };
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct MockTransaction {
-    signer: AccountId,
-    action: String,
+pub struct Args {
+    account: AccountId,
+    number_big: U64,
+    number_small: u8,
+    point: Point,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 #[serde(crate = "near_sdk::serde")]
-pub struct MockBlock {
-    author: AccountId,
-    height: U64,
-    transactions: Vec<MockTransaction>,
+pub struct MockStructure {
+    big: U64,
+    small: u8,
+    vector: Vec<U64>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct MockReturn {
+    account: String,
+    x: i32,
+    structure: MockStructure,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Point {
+    x: i32,
+    y: i32,
 }
 
 #[near_bindgen]
@@ -32,28 +49,34 @@ impl Contract {
     pub fn evaluate_complex_input(&mut self, contract_account_id: AccountId) -> Promise {
         self.assert_valid_account(&contract_account_id);
 
-        let validator = format!("validator{}.testnet", self.random_u8(1));
-
-        let transactions = vec![
-            MockTransaction {
-                signer: "alice.testnet".parse().unwrap(),
-                action: "FunctionCall".to_string(),
-            },
-            MockTransaction {
-                signer: "bob.testnet".parse().unwrap(),
-                action: "Transfer".to_string(),
-            },
-        ];
-
-        let args = json!({"author": validator, "height": U64(env::block_height()), "transactions": transactions})
-            .to_string()
-            .into_bytes();
-
-        let expected_mock_block = MockBlock {
-            author: validator.parse().unwrap(),
-            height: U64(env::block_height()),
-            transactions: transactions,
+        let account_id = env::signer_account_id();
+        let number_big = U64(env::block_height());
+        let number_small = self.random_u8(0);
+        let point = Point {
+            x: i32::from(self.random_u8(1)),
+            y: i32::from(self.random_u8(2)),
         };
+
+        let number = number_big.0 - 1;
+
+        let expected = MockReturn {
+            account: account_id.to_string(),
+            x: point.x,
+            structure: MockStructure {
+                big: number_big,
+                small: number_small,
+                vector: vec![number_big, U64::from(number)],
+            },
+        };
+
+        let args = json!(Args {
+            account: account_id,
+            number_big,
+            number_small,
+            point
+        })
+        .to_string()
+        .into_bytes();
 
         Promise::new(contract_account_id.clone())
             .function_call(
@@ -65,24 +88,24 @@ impl Contract {
             .then(
                 Self::ext(env::current_account_id())
                     .with_static_gas(Gas(5 * TGAS))
-                    .eval_ci_callback(env::predecessor_account_id(), expected_mock_block),
+                    .eval_ci_callback(env::predecessor_account_id(), expected),
             )
     }
 
     #[private]
     pub fn eval_ci_callback(
         &mut self,
-        #[callback_result] call_result: Result<MockBlock, PromiseError>,
+        #[callback_result] call_result: Result<MockReturn, PromiseError>,
         student_id: AccountId,
-        expected_mock_block: MockBlock,
+        expected: MockReturn,
     ) -> bool {
         match call_result {
-            Ok(resulted_block) => {
+            Ok(result) => {
                 log_str(&format!(
-                    "Expected block to be {:#?}, received {:#?}",
-                    expected_mock_block, resulted_block
+                    "Expected structure to be {:#?}, received {:#?}",
+                    expected, result
                 ));
-                require!(resulted_block.eq(&expected_mock_block));
+                require!(result.eq(&expected));
 
                 let mut evaluations = self.evaluations.get(&student_id).unwrap();
                 evaluations[3] = true;
